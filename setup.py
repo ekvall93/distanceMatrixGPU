b972dfec4bdc4ac3b02d3beefcf4d0aaf7ca20a8
Markus Ekvall <marekv@kth.se>
2019-06-16 22:44:41 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='distanceMatrixGPU',
+    version='0.0.1',
+    description='Fast distance matrix calculation',
+    url='https://github.com/ekvall93/distanceMatrixGPU.git',
+    author='Markus Ekvall',
+    author_email='marekv@kth.se',
+    license='mitlicense',
+    packages=['distanceMatrixGPU'],
+    zip_safe=False
+)
